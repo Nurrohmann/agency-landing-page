470c7129f95a78318ee02a9cc90c0ff3577eeae7
nurr <nurr@pop-os.localdomain>
2023-04-25 11:33:46 +0700

    update content

--- src/component/Subscribe.jsx
+++ src/component/Subscribe.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 function Subscribe() {
    return (
-      <div>
-         <div></div>
+      <div className='subscribe-container'>
+         <div className='subscribe-items'>
+            <p className='p1'>NEWSLETTER</p>
+            <h1 className='subscribe'>Subscribe Our Newsletter</h1>
+            <p className='p2'>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+               explicabo voluptas nisi iste reiciendis, tempore commodi
+               necessitatibus, laborum laudantium.
+            </p>
+            <div className='input'>
+               <input type='text' placeholder={"Enter email..."} />
+               <button>Subscribe</button>
+            </div>
+         </div>
       </div>
    );
 }
